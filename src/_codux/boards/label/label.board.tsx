bd00447597e2d31d2e84cab498d11f4e5f779e62
pilarbuchen <pilarb@wix.com>
2023-11-29 12:57:01 -0600

    removing unesessary folder and files

--- src/_codux/boards/label/label.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import  LabelPointing from '../../../components/label/label';
-
-export default createBoard({
-    name: 'Label',
-    Board: () => <LabelPointing />,
-    isSnippet: true,
-});
